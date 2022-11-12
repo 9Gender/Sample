b53f90ee0841a1b3c8a3ec6e2fc12704dd8dde17
asus <asus@MM18371>
2022-11-12 10:39:41 +0530

    Pushing project into git

---
+++ jenkins/src/main/java/bcse/jenkins/App.java
@@ -0,0 +1,13 @@
+package bcse.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Jenkins World" );
+    }
+}
